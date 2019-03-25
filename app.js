27f887bfb390512209cb6270abff68c44307248d
peppe-quint-boogaard <peppequintb@gmail.com>
2019-03-25 13:17:13 +0100

    ejs template

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// TODO: progressive enhancement and feature detection has to be in concept
-// TODO: express server with post method form
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -7,22 +5,24 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 2000;
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const score = ["Tst1", "Tst2"];
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public/views"));
 
 app.post("/score", (req, res) => {
-  const newScore = req.body.newScore;
-
-  score.push(newScore);
-  console.log("Test");
+  const scoreboard = {
+    home: 0,
+    away: 0,
+    game: 1
+  };
 
   res.redirect("/");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello scoreboard");
+  res.render("pages/index");
 });
 
 app.listen(port, () => console.log(`Live scoreboard running on port ${port}.`));
