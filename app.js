ae888ac2e5cd50983a614557ef3ea8ebe5f014bb
peppe-quint-boogaard <peppequintb@gmail.com>
2019-03-25 16:53:58 +0100

    every button new post method

--- app.js
+++ app.js
@@ -11,18 +11,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "public/views"));
 
-app.post("/score", (req, res) => {
-  const scoreboard = {
-    home: 0,
-    away: 0,
-    game: 1
-  };
+const scoreboard = {
+  home: 0,
+  away: 0,
+  game: 1,
+  time: "0:00"
+};
 
+app.get("/", (req, res) => {
+  res.render("pages/index", { scoreboard: scoreboard });
+});
+
+app.post("/homescoreminus", (req, res) => {
+  scoreboard.home--;
+  console.log(scoreboard.home);
   res.redirect("/");
 });
 
-app.get("/", (req, res) => {
-  res.render("pages/index");
+app.post("/homescoreplus", (req, res) => {
+  scoreboard.home++;
+  console.log(scoreboard.home);
+  res.redirect("/");
+});
+
+app.post("/awayscoreminus", (req, res) => {
+  scoreboard.away--;
+  console.log(scoreboard.away);
+  res.redirect("/");
+});
+
+app.post("/awayscoreplus", (req, res) => {
+  scoreboard.away++;
+  console.log(scoreboard.away);
+  res.redirect("/");
 });
 
 app.listen(port, () => console.log(`Live scoreboard running on port ${port}.`));
