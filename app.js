43ad2c1fe21201bf0e1b97ca766e60c274e7039c
peppe-quint-boogaard <peppequintb@gmail.com>
2019-03-29 12:02:02 +0100

    heroku fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 2000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
